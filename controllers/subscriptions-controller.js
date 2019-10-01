0ac23a940bb05bde19fb37153adcb1dc9850b88d
Billi Ramirez <ramirezbilly42@outlook.com>
2019-10-01 10:01:16 -0600

    Express initial commit with some work

---
+++ controllers/subscriptions-controller.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const asyncWrapper = require('../utilities/async-wrapper').AsyncWrapper;
+//   api/subscriptions
+
+router.get("/", asyncWrapper(async (req, res) => {
+
+}));
+
+router.get("/:id", asyncWrapper(async (req, res) => {
+
+}));
+
+router.post("/", asyncWrapper(async (req, res) => {
+
+}));
+
+router.delete("/:id", asyncWrapper(async (req, res) => {
+
+}));
+
+module.exports = router;
