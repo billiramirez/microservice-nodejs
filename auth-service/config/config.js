51b55a2f2ba35ccfb1f57acf1fb7794734177f00
Billi Ramirez <ramirezbilly42@outlook.com>
2019-10-03 07:46:40 -0600

    Creating user model and migration

---
+++ auth-service/config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  development: {
+    username: "root",
+    password: "123456789",
+    database: "Users",
+    host: "localhost",
+    dialect: "mysql",
+    port: "3309"
+    },
+  production: {
+    username: "root",
+    password: "123456789",
+    database: "Users",
+    host: "localhost",
+    dialect: "mysql",
+    port: "3309"
+    }
+  }
