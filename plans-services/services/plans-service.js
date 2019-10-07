1e9070ee2f3b30b271d7a6cb2a18e073619f1939
Billi Ramirez <ramirezbilly42@outlook.com>
2019-10-07 11:17:40 -0600

    Adding cache with redis

--- plans-services/services/plans-service.js
+++ plans-services/services/plans-service.js
@@ -1,8 +1,25 @@
 const Plan = require("../models/index")["Plan"];
-
+const CachingService = require("../services/caching-service");
 module.exports = class PlansService {
+
+  constructor(){
+    this.cachingService = new CachingService({
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASSWORD
+    });
+  }
+
   async findAll(userId){
-    return await Plan.findAll({ where: { userId }});
+    let plans = await this.cachingService.getPlans(userId);
+    if (plans){
+      return plans;
+    }else {
+      let plans = await Plan.findAll({ where: { userId }});
+      // store to the cache;
+      await this.cachingService.purgeCache(userId);
+      return plans;
+    }
   }
 
   async findOne(id){
